a97be0a48aa7668a860b08cebcc574886bffbeaf
yiwei <yiwei.ge@spaceconcordia.ca>
2017-01-31 19:06:07 -0500

    Add tests for GET/ todos

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos',(req,res)=>{
 
 });
 
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status.send(e);
+  });
+});
+
 app.listen(3000, ()=>{
   console.log('started on port 3000');
 })
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text:"first test todo"
+},{
+  text:"second test todo"
+}];
 beforeEach((done)=>{
-  Todo.remove({}).then(()=>done());
-})
+  Todo.remove({}).then(()=>{
+    return Todo.insertMany(todos);
+
+  }).then(()=>done());
+});
 
 describe('POST /todo',()=>{
   it('should create new todo',(done)=>{
@@ -22,7 +30,7 @@ describe('POST /todo',()=>{
       if(err){
         return done(err);
       }
-      Todo.find().then((todos)=>{
+      Todo.find({text}).then((todos)=>{
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
         done();
@@ -45,9 +53,21 @@ describe('POST /todo',()=>{
         return done(err);
       }
       Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e)=>done(e));
     })
+  })
+})
+
+describe('GET /todos',()=>{
+  it('should get all todos',(done)=>{
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   })
 })
