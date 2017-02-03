d4d8538d87486d538bb5d210b010d00a53bf3ecd
yiwei <yiwei.ge@spaceconcordia.ca>
2017-02-02 20:57:06 -0500

    private middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 console.log('user.js');
 var UserSchema = new mongoose.Schema({
   email:{
@@ -55,6 +56,19 @@ UserSchema.statics.findByToken = function (token) {
       'tokens.access' : 'auth'
     })
 }
+UserSchema.pre('save',function (next) {
+  var user = this;
+  if(user.isModified('password')){
+    bcrypt.genSalt(10,(err,salt)=>{
+      bcrypt.hash(user.password,salt,(err,hash)=>{
+        user.password = hash;
+        next();
+      })
+    })
+  }else {
+    next();
+  }
+})
 
 var User = mongoose.model('User',UserSchema);
 
